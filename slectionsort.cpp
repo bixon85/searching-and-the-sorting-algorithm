65383ca61f4b2adb771228549895b5743b5acda9
bixon85 <155192381+bixon85@users.noreply.github.com>
2025-01-03 21:29:14 +0530

    this is the selection sort algo

--- slectionsort.cpp
+++ slectionsort.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
-    
+    vector<int> arr = {5, 2, 6, 1, 3, 4};
+    int min;
+    for(int i = 0; i < arr.size(); i++){
+        min = i;
+        for(int j = i+1; j < arr.size(); j++){
+            if(arr[j] < arr[min]){
+                min = j;
+            }
+        }
+        swap(arr[i], arr[min]);
+    }
     return 0;
 }
